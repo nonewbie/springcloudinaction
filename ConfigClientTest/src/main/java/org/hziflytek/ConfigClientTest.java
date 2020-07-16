8607a164ed5e4f509fbb5b63a8de45390020e968
xtli3 <xtli3@iflytek.com>
2020-07-17 00:49:26 +0800

    新增配置中心，本地配置文件，单节点

---
+++ ConfigClientTest/src/main/java/org/hziflytek/ConfigClientTest.java
@@ -0,0 +1,13 @@
+package org.hziflytek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientTest.class, args);
+    }
+
+}
